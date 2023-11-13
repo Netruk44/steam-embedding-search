987e21e7f66907139a80d429f20c8dfb83539006
Daniel Perry <dperry@danieltperry.me>
2023-11-13 18:23:48 -0500

    04_querydataset implementation

---
+++ 04_querydataset/run.py
@@ -0,0 +1,148 @@
+
+import click
+from instructor_model import InstructorModel
+#from sqlite_helpers import * # TODO: Define functions
+import sqlite_helpers
+import tqdm
+import logging
+import os
+from typing import List
+import numpy as np
+
+COLOR_DARK_GREY = "\x1b[38;5;240m"
+COLOR_BOLD = "\x1b[1m"
+COLOR_RESET = "\x1b[0m"
+LOGGING_FORMAT = COLOR_DARK_GREY + '[%(asctime)s - %(name)s]' + COLOR_RESET + COLOR_BOLD + ' %(levelname)s:' + COLOR_RESET + ' %(message)s'
+
+@click.command()
+@click.option('--db', required=True, help='Path to SQLite database')
+@click.option('--query', required=True, help='Query to search for')
+@click.option('--index', default=None, help='Path to index file')
+@click.option('--embed-query', default='Represent a video game that has a description of:', help='Embedding instruction for query')
+@click.option('--verbose', is_flag=True, help='Print verbose output')
+def main(db, query, index, embed_query, verbose):
+    logging.basicConfig(format = LOGGING_FORMAT, level = logging.INFO if not verbose else logging.DEBUG)
+
+    # Load input sqlite database
+    if not os.path.exists(db):
+        logging.error(f"Input SQLite database {db} does not exist")
+        exit(1)
+    
+    conn = sqlite_helpers.create_connection(db)
+
+    # Make sure input tables exist
+    if not sqlite_helpers.check_input_db_tables(conn):
+        logging.error(f"Input SQLite database {db} does not have the required tables")
+        exit(1)
+    
+    # Load instructor model
+    # Needs to match the model used to generate the embeddings
+    instructor = InstructorModel(
+        # Models:
+        # - hkunlp/instructor-large : ~2.5 GB VRAM
+        # - hkunlp/instructor-xl    : ~6 GB VRAM
+        model_name = "hkunlp/instructor-large",
+    )
+
+    # Get query embedding
+    query_tokenized = instructor.tokenize(query)
+    if len(query) > instructor.get_max_query_chunk_length():
+        logging.error(f"Query is too long. {len(query_tokenized)} / {instructor.get_max_query_chunk_length()} tokens are used.")
+        exit(1)
+
+    instructor.embedding_instruction = embed_query
+    query_embed: List[float] = instructor.generate_embedding_for_query(query, verbose)
+
+    if index is not None:
+        # TODO: Load FAISS index
+        results = []
+    else:
+        # Need to do a linear search
+        results = slow_search(conn, query_embed, max_results=10)
+    '''
+    results = [
+        {
+            'appid': 123,
+            'name': 'Game Name',
+            'match_type': 'description',
+            'score': 0.555,
+        },
+        {
+            'appid': 456,
+            'name': 'Game Name 2',
+            'match_type': 'review',
+            'score': 0.444,
+        }
+    ]
+    '''
+
+    # Display results
+    print(f"Results for query: {query}")
+    for result in results:
+        print(f"  {result['appid']}: {result['name']} ({result['match_type']})")
+        print(f"    Score: {result['score']}")
+    
+    # Close connection
+    conn.close()
+
+def cosine_similarity(a: List[float], b: List[float]) -> float:
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def compare_all_embeddings_take_max(embeddings: List[List[float]], query_embed: List[float]) -> float:
+    similarities = [cosine_similarity(embedding, query_embed) for embedding in embeddings]
+    return max(similarities)
+
+def add_to_capped_list(list_to_add_to: List[dict], item_to_add: dict, max_length: int):
+    list_to_add_to.append(item_to_add)
+    if len(list_to_add_to) > max_length:
+        # Find the lowest score, remove it
+        lowest_score = min(list_to_add_to, key=lambda x: x['score'])
+        list_to_add_to.remove(lowest_score)
+
+def slow_search(conn, query_embed, max_results=10):
+    matches = []
+
+    # Store Description Search
+    bar = tqdm.tqdm(total=sqlite_helpers.get_count_embeddings_for_descriptions(conn), desc="Store Descriptions")
+
+    for current_page in sqlite_helpers.get_paginated_embeddings_for_descriptions(conn, page_size=100):
+        for appid, embeddings in current_page.items():
+            score = compare_all_embeddings_take_max(embeddings, query_embed)
+            name = sqlite_helpers.get_name_for_appid(conn, appid)
+
+            add_to_capped_list(matches, {
+                'appid': appid,
+                'name': name,
+                'match_type': 'description',
+                'score': score,
+            }, max_results)
+
+            bar.update(1)
+    
+    bar.close()
+    
+    # Review search
+    bar = tqdm.tqdm(total=sqlite_helpers.get_count_embeddings_for_reviews(conn), desc="Reviews")
+
+    for current_page in sqlite_helpers.get_paginated_embeddings_for_reviews(conn, page_size=100):
+        for recommendationid, embeddings in current_page.items():
+            score = compare_all_embeddings_take_max(embeddings, query_embed)
+            appid = sqlite_helpers.get_appid_for_recommendationid(conn, recommendationid)
+            name = sqlite_helpers.get_name_for_appid(conn, appid)
+
+            add_to_capped_list(matches, {
+                'appid': appid,
+                'name': name,
+                'match_type': 'review',
+                'score': score,
+            }, max_results)
+
+            bar.update(1)
+    
+    bar.close()
+
+
+    return matches
+
+if __name__ == '__main__':
+    main()
---
+++ 04_querydataset/sqlite_helpers.py
@@ -0,0 +1,230 @@
+import sqlite3
+import logging
+from typing import List, Optional, Set, Dict, Generator
+import pickle
+
+## Init
+
+def create_connection(db_file: str = "steam.db") -> sqlite3.Connection:
+    """
+    Creates a connection to the SQLite database.
+
+    Args:
+        db_file (str): The path to the SQLite database.
+
+    Returns:
+        sqlite3.Connection: A connection to the SQLite database.
+    """
+    logging.debug("Creating connection to SQLite database")
+    return sqlite3.connect(db_file)
+
+
+## Validation
+
+def check_table(conn: sqlite3.Connection, table_name: str) -> bool:
+    """
+    Checks if the table exists in the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        table_name (str): The name of the table to check for.
+
+    Returns:
+        bool: True if the table exists, False otherwise.
+    """
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'
+    ''')
+    table_exists = c.fetchone()[0] == 1
+
+    c.close()
+
+    logging.debug(f"Table {table_name} exists: {table_exists}")
+
+    return table_exists
+
+def check_input_db_tables(conn: sqlite3.Connection) -> bool:
+    """
+    Checks if the required tables exist in the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        bool: True if the tables exist, False otherwise.
+    """
+    logging.debug(f"Checking if required tables exist in input SQLite database.")
+    required_tables = ["description_embeddings", "review_embeddings", "gamelist"]
+    
+    tables_exist = [check_table(conn, table_name) for table_name in required_tables]
+    
+    return all(tables_exist)
+
+
+## Data retrieval
+
+def get_all_embeddings_for_descriptions(conn: sqlite3.Connection) -> Dict[int,List[List[float]]]:
+    """
+    Gets all the embeddings for store descriptions from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        Dict[int, List[List[float]]]: A dictionary mapping appid to the embedding for the game description.
+    """
+    logging.debug(f"Getting all game description embeddings from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT appid, embedding FROM description_embeddings
+    ''')
+    results = c.fetchall()
+
+    c.close()
+
+    return {appid: pickle.loads(embedding) for appid, embedding in results}
+
+def get_count_embeddings_for_descriptions(conn: sqlite3.Connection) -> int:
+    """
+    Gets the number of embeddings for store descriptions from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        int: The number of embeddings for game descriptions.
+    """
+    logging.debug(f"Getting count of game description embeddings from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT count(*) FROM description_embeddings
+    ''')
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
+
+def get_paginated_embeddings_for_descriptions(conn: sqlite3.Connection, page_size = 100) -> Generator[Dict[int,List[List[float]]], None, None]:
+    """
+    Gets all the embeddings for store descriptions from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        Dict[int, List[List[float]]]: A dictionary mapping appid to the embedding for the game description.
+    """
+    logging.debug(f"Getting all game description embeddings from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT appid, embedding FROM description_embeddings
+    ''')
+
+    while True:
+        results = c.fetchmany(page_size)
+        if not results:
+            break
+        yield {appid: pickle.loads(embedding) for appid, embedding in results}
+
+    c.close()
+
+def get_count_embeddings_for_reviews(conn: sqlite3.Connection) -> int:
+    """
+    Gets the number of embeddings for reviews from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        int: The number of embeddings for reviews.
+    """
+    logging.debug(f"Getting count of review embeddings from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT count(*) FROM review_embeddings
+    ''')
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
+
+
+def get_paginated_embeddings_for_reviews(conn: sqlite3.Connection, page_size = 100) -> Generator[Dict[int,List[List[float]]], None, None]:
+    """
+    Gets all the embeddings for reviews from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        Dict[int, List[List[float]]]: A dictionary mapping recommendationid to the embedding for the review.
+    """
+    logging.debug(f"Getting all review embeddings from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT recommendationid, embedding FROM review_embeddings
+    ''')
+
+    while True:
+        results = c.fetchmany(page_size)
+        if not results:
+            break
+        yield {recommendationid: pickle.loads(embedding) for recommendationid, embedding in results}
+
+    c.close()
+
+
+def get_appid_for_recommendationid(conn: sqlite3.Connection, recommendationid: int) -> int:
+    """
+    Gets the appid for the given recommendationid from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        recommendationid (int): The recommendationid to get the appid for.
+
+    Returns:
+        int: The appid for the given recommendationid.
+    """
+    logging.debug(f"Getting appid for recommendationid {recommendationid} from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT appid FROM appreviews WHERE recommendationid = ?
+    ''', (recommendationid,))
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
+
+def get_name_for_appid(conn: sqlite3.Connection, appid: int) -> str:
+    """
+    Gets the name for the given appid from the input SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        appid (int): The appid to get the name for.
+
+    Returns:
+        str: The name for the given appid.
+    """
+    logging.debug(f"Getting name for appid {appid} from input SQLite database.")
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT name FROM gamelist WHERE appid = ?
+    ''', (appid,))
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
