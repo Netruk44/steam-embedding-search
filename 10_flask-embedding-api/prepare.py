7de73f90b9802bb19edca0a87855f4a1f226fec9
Daniel Perry <dperry@danieltperry.me>
2023-11-15 20:06:53 -0500

    Add prepare script

---
+++ 10_flask-embedding-api/prepare.py
@@ -0,0 +1,13 @@
+
+# Prepare machine for running model (run during docker build)
+
+from instructor_model import InstructorModel
+from app import instructor_model_name
+import sys
+
+# Load instructor model
+instructor = InstructorModel(
+    model_name = instructor_model_name,
+)
+
+sys.exit(0)
