9179983b84ed097dcb71e3e718b7d7762b0ffe7e
Daniel Perry <dperry@danieltperry.me>
2023-11-15 21:33:52 -0500

    Rework things for faster Docker builds

---
+++ 10_flask-embedding-api/config.py
@@ -0,0 +1,4 @@
+
+
+database_path = 'steam_instructor-xl.db'
+instructor_model_name = 'hkunlp/instructor-xl'
--- 10_flask-embedding-api/prepare.py
+++ 10_flask-embedding-api/prepare.py
@@ -2,7 +2,7 @@
 # Prepare machine for running model (run during docker build)
 
 from instructor_model import InstructorModel
-from app import instructor_model_name
+from config import instructor_model_name
 import sys
 
 # Load instructor model
