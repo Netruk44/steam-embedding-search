b371006585b65e2bde0040e2c18be9d44432c24d
Daniel Perry <dperry@danieltperry.me>
2023-11-15 22:36:46 -0500

    Update template with search app code

--- 11_react-interface/src/App.js
+++ 11_react-interface/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    fetch(`http://home.danieltperry.me:5000/get_results?query=${encodeURIComponent(searchTerm)}`)
+      .then(response => response.json())
+      .then(data => setSearchResults(data))
+      .catch(error => console.error('Error:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Steam ✨Vibe✨ Search</h1>
+      <p>Describe the kind of game you're looking for, see what you get!</p>
+      <form onSubmit={handleSearch}>
+        <label htmlFor="search">Search:</label>
+        <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      <hr />
+      <h2>Results</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>App ID</th>
+            <th>Icon</th>
+            <th>Name</th>
+            <th>Match Type</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map((result, index) => (
+            <tr key={index}>
+              <td>{result.appid}</td>
+              <td><img src={`https://cdn.akamai.steamstatic.com/steam/apps/${result.appid}/header.jpg`} style={{ maxWidth: '240px', height: 'auto' }} /></td>
+              <td><a href={`https://store.steampowered.com/app/${result.appid}`}>{result.name}</a></td>
+              <td>{result.match_type}</td>
+              <td>{result.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
