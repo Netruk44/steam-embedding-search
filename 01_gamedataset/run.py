72db83bb18addb9372f5e9cdec15a9af702caa01
Daniel Perry <dperry@danieltperry.me>
2023-11-18 16:49:31 -0500

    Add support for specifying which item types to update

--- 01_gamedataset/run.py
+++ 01_gamedataset/run.py
@@ -15,9 +15,14 @@ LOGGING_FORMAT = COLOR_DARK_GREY + '[%(asctime)s - %(name)s]' + COLOR_RESET + CO
 @click.option('--new', is_flag=True, help='Create a new database instead of updating an existing one')
 @click.option('--limit', default=5000, help='Limit the number of appids to update')
 @click.option('--update-all', is_flag=True, help='Ignore limit, update all appids')
+@click.option('--update-type', default='all', help='Type of appids to update (all, details, reviews)')
 @click.option('--verbose', is_flag=True, help='Print verbose output')
-def main(db, new, limit, update_all, verbose):
+def main(db, new, limit, update_all, update_type, verbose):
     logging.basicConfig(format = LOGGING_FORMAT, level = logging.INFO if not verbose else logging.DEBUG)
+    
+    if update_type not in ["all", "details", "reviews"]:
+        logging.error("Invalid update type. Must be one of: all, details, reviews")
+        exit(1)
 
     # Create SQLite database
     if new:
@@ -48,36 +53,38 @@ def main(db, new, limit, update_all, verbose):
     appids_to_update_details = sqlite_helpers.get_appdetails_to_update(conn, count = limit)
     
     # Update app details
-    bar = tqdm.tqdm(appids_to_update_details, desc = "Updating app details", smoothing = 0.0)
-    for appid in bar:
-        bar.set_postfix(appid=str(appid))
-        try:
-            appdetails = get_app_details(appid)
-            sqlite_helpers.insert_appdetails(conn, appid, appdetails)
-        except KeyboardInterrupt:
-            raise
-        except:
-            logging.warning("Failed to get app details for appid " + str(appid) + ". Skipping...")
-            continue
-        finally:
-            sqlite_helpers.mark_appdetails_updated(conn, appid)
+    if update_type == "all" or update_type == "details":
+        bar = tqdm.tqdm(appids_to_update_details, desc = "Updating app details", smoothing = 0.0)
+        for appid in bar:
+            bar.set_postfix(appid=str(appid))
+            try:
+                appdetails = get_app_details(appid)
+                sqlite_helpers.insert_appdetails(conn, appid, appdetails)
+            except KeyboardInterrupt:
+                raise
+            except:
+                logging.warning("Failed to get app details for appid " + str(appid) + ". Skipping...")
+                continue
+            finally:
+                sqlite_helpers.mark_appdetails_updated(conn, appid)
 
 
     # Update app reviews
-    appids_to_update_reviews = sqlite_helpers.get_appreviews_to_update(conn, output_count = limit)
+    if update_type == "all" or update_type == "reviews":
+        appids_to_update_reviews = sqlite_helpers.get_appreviews_to_update(conn, output_count = limit)
 
-    bar = tqdm.tqdm(appids_to_update_reviews, desc = "Getting app reviews", smoothing = 0.0)
-    for appid in bar:
-        bar.set_postfix(appid=str(appid))
+        bar = tqdm.tqdm(appids_to_update_reviews, desc = "Getting app reviews", smoothing = 0.0)
+        for appid in bar:
+            bar.set_postfix(appid=str(appid))
 
-        try:
-            known_reviews = sqlite_helpers.get_appreview_recommendationids(conn, appid)
-            appreviews = get_n_reviews(appid, n = 100)
-            new_appreviews = [review for review in appreviews if int(review["recommendationid"]) not in known_reviews]
-            logging.debug("Found " + str(len(new_appreviews)) + " new reviews for appid " + str(appid) + ".")
-            sqlite_helpers.insert_appreviews(conn, appid, new_appreviews)
-        finally:
-            sqlite_helpers.mark_appreviews_updated(conn, appid)
+            try:
+                known_reviews = sqlite_helpers.get_appreview_recommendationids(conn, appid)
+                appreviews = get_n_reviews(appid, n = 100)
+                new_appreviews = [review for review in appreviews if int(review["recommendationid"]) not in known_reviews]
+                logging.debug("Found " + str(len(new_appreviews)) + " new reviews for appid " + str(appid) + ".")
+                sqlite_helpers.insert_appreviews(conn, appid, new_appreviews)
+            finally:
+                sqlite_helpers.mark_appreviews_updated(conn, appid)
 
     conn.close()
 
