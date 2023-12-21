e42a6e326fd11f48445dc56c1e8394a7024396a2
Daniel Perry <dperry@danieltperry.me>
2023-12-21 17:16:13 -0500

    Implement adding hnsw index to database

---
+++ 03_hnsw-index/run.py
@@ -0,0 +1,129 @@
+import click
+import sqlite_helpers
+import tqdm
+import logging
+import hnswlib
+import numpy as np
+from typing import List, Optional, Set, Dict, Iterator, Tuple, Callable
+import sqlite3
+import os
+
+
+COLOR_DARK_GREY = "\x1b[38;5;240m"
+COLOR_BOLD = "\x1b[1m"
+COLOR_RESET = "\x1b[0m"
+LOGGING_FORMAT = COLOR_DARK_GREY + '[%(asctime)s - %(name)s]' + COLOR_RESET + COLOR_BOLD + ' %(levelname)s:' + COLOR_RESET + ' %(message)s'
+
+@click.command()
+@click.option('--db', required=True, help='Path to SQLite database')
+@click.option('--index-type', type=click.Choice(['description', 'review', 'all']), default='all', help='Type of index to create')
+@click.option('--remove-old-indexes', default=False, is_flag=True, help='Remove old indexes after creating new ones')
+@click.option('--verbose', is_flag=True, help='Print verbose output')
+def main(db, index_type, remove_old_indexes, verbose):
+  logging.basicConfig(format = LOGGING_FORMAT, level = logging.INFO if not verbose else logging.DEBUG)
+
+  # Load input sqlite database
+  if not os.path.exists(db):
+    logging.error(f"Input SQLite database {db} does not exist")
+    exit(1)
+  
+  conn = sqlite_helpers.create_connection(db)
+
+  # Make sure input tables exist
+  input_tables = ['description_embeddings', 'review_embeddings']
+
+  if not sqlite_helpers.check_all_tables_exist(conn, input_tables):
+    logging.error(f"Input SQLite database {db} does not have the required tables")
+    logging.info(f"Required tables: {input_tables}")
+    exit(1)
+  
+  # Make sure output tables exist
+  sqlite_helpers.create_output_db_tables(conn)
+
+  # Create and insert new indexes
+  # ef and M values were found through experimentation with playground.ipynb notebook
+  # M value doesn't seem to affect much for these embeddings.
+  if index_type == 'description' or index_type == 'all':
+    logging.info("Creating new description index...")
+    description_index = create_index(
+      conn, 
+      num_elements=sqlite_helpers.get_count_appids_with_description_embeddings(conn), 
+      ef=2000, 
+      M=28, 
+      get_batches=get_descriptions_by_appid_batched
+    )
+    sqlite_helpers.add_description_index(conn, description_index)
+
+  if index_type == 'review' or index_type == 'all':
+    logging.info("Creating new review index...")
+    review_index = create_index(
+      conn, 
+      num_elements=sqlite_helpers.get_count_appids_with_review_embeddings(conn), 
+      ef=15000, 
+      M=28, 
+      get_batches=get_reviews_by_appid_batched
+    )
+    sqlite_helpers.add_review_index(conn, review_index)
+
+  if remove_old_indexes:
+    sqlite_helpers.remove_old_indexes(conn)
+    logging.info("Removed old indexes, remember to VACUUM the database to reclaim space")
+  
+  conn.close()
+
+
+def mean_pooling(embeddings: List[List[float]]) -> List[float]:
+  return np.sum(embeddings, axis=0) / len(embeddings)
+
+def get_index_dimension(conn: sqlite3.Connection) -> int:
+  return len(sqlite_helpers.get_any_description_embeddings_list(conn)[0])
+
+def pool_description_embeddings(embeddings: List[List[float]]) -> List[float]:
+  # embeddings[chunk_size][embedding_dim]
+  return mean_pooling(embeddings)
+
+def pool_review_embeddings(embeddings: Dict[str, List[List[float]]]) -> List[float]:
+  # embeddings[review_id][chunk_size][embedding_dim]
+  flat_embeddings = [review_embedding for review_id in embeddings for review_embedding in embeddings[review_id]]
+  # flat_embeddings[chunk_size][embedding_dim]
+  return mean_pooling(flat_embeddings)
+
+def get_descriptions_by_appid_batched(conn: sqlite3.Connection, page_size: int = 1000) -> Iterator[List[Tuple[int, List[float]]]]:
+  for batch in sqlite_helpers.get_description_embeddings_batch(conn, page_size):
+    appids, embeddings = zip(*batch)
+    yield [(appid, pool_description_embeddings(embedding)) for appid, embedding in zip(appids, embeddings)]
+
+def get_reviews_by_appid_batched(conn: sqlite3.Connection, page_size: int = 1000) -> Iterator[List[Tuple[int, List[float]]]]:
+  appids = sqlite_helpers.get_appids_with_reviews(conn)
+
+  for i in range(0, len(appids), page_size):
+    yield [(appid, pool_review_embeddings(sqlite_helpers.get_review_embeddings_for_appid(conn, appid))) for appid in appids[i:i+page_size]]
+
+
+
+def create_index(
+  conn: sqlite3.Connection, 
+  num_elements: int, 
+  ef: int, 
+  M: int, 
+  get_batches: Callable[[sqlite3.Connection, int], Iterator[List[Tuple[int, List[float]]]]]) -> hnswlib.Index:
+  dim = get_index_dimension(conn)
+  logging.info(f"Creating index with: {num_elements} elements, dim={dim}, ef={ef}, M={M}")
+  
+  index = hnswlib.Index(space='cosine', dim=dim)
+  index.init_index(max_elements=num_elements, ef_construction=ef, M=M)
+  index.set_ef(ef)
+
+  bar = tqdm.tqdm(total=num_elements, desc="Creating index", smoothing=0.9)
+
+  for batch in get_batches(conn):
+    appids, embeddings = zip(*batch)
+    index.add_items(embeddings, appids)
+    bar.update(len(batch))
+  
+  bar.close()
+
+  return index
+
+if __name__ == '__main__':
+  main()
---
+++ 03_hnsw-index/sqlite_helpers.py
@@ -0,0 +1,300 @@
+import sqlite3
+import logging
+from typing import List, Optional, Set, Dict, Iterator, Tuple
+import pickle
+import hnswlib
+
+def create_connection(db_file: str = "steam.db") -> sqlite3.Connection:
+    """
+    Creates a connection to the SQLite database.
+
+    Args:
+        db_file (str): The path to the SQLite database.
+
+    Returns:
+        sqlite3.Connection: A connection to the SQLite database.
+    """
+    logging.debug("Creating connection to SQLite database")
+    return sqlite3.connect(db_file)
+
+def check_table(conn: sqlite3.Connection, table_name: str) -> bool:
+    """
+    Checks if the table exists in the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        table_name (str): The name of the table to check for.
+
+    Returns:
+        bool: True if the table exists, False otherwise.
+    """
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}'
+    ''')
+    table_exists = c.fetchone()[0] == 1
+
+    c.close()
+
+    logging.debug(f"Table {table_name} exists: {table_exists}")
+
+    return table_exists
+
+def check_all_tables_exist(conn: sqlite3.Connection, tables: List[str]) -> bool:
+    """
+    Checks if all tables exist in the SQLite database.
+
+    Args:
+        tables (List[str]): A list of table names to check for.
+    
+    Returns:
+        bool: True if all tables exist, False otherwise.
+    """
+    
+    return all([check_table(conn, table) for table in tables])
+
+def create_output_db_tables(conn: sqlite3.Connection):
+    """
+    Creates the output tables in the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+    """
+    logging.debug("Creating output tables in SQLite database")
+
+    c = conn.cursor()
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS description_embeddings_hnsw_index (
+            index_id INTEGER PRIMARY KEY,
+            creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            pickle BLOB NOT NULL
+        )
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS review_embeddings_hnsw_index (
+            index_id INTEGER PRIMARY KEY,
+            creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            pickle BLOB NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    c.close()
+
+def remove_old_indexes(conn: sqlite3.Connection):
+    """
+    Removes old indexes from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+    """
+    logging.debug("Removing old indexes from SQLite database")
+
+    c = conn.cursor()
+
+    c.execute('''
+        DELETE FROM description_embeddings_hnsw_index
+        WHERE index_id NOT IN (
+            SELECT index_id 
+            FROM description_embeddings_hnsw_index 
+            ORDER BY creation_time DESC 
+            LIMIT 1
+        )
+    ''')
+    logging.debug(f"Removed {c.rowcount} old description indexes")
+
+    c.execute('''
+        DELETE FROM review_embeddings_hnsw_index
+        WHERE index_id NOT IN (
+            SELECT index_id 
+            FROM review_embeddings_hnsw_index 
+            ORDER BY creation_time DESC 
+            LIMIT 1
+        )
+    ''')
+    logging.debug(f"Removed {c.rowcount} old review indexes")
+
+    conn.commit()
+    c.close()
+
+def add_description_index(conn: sqlite3.Connection, index: hnswlib.Index):
+    """
+    Adds a description index to the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        index (hnswlib.Index): The index to add.
+    """
+    logging.debug("Adding description index to SQLite database")
+
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO description_embeddings_hnsw_index (pickle)
+        VALUES (?)
+    ''', (pickle.dumps(index),))
+
+    conn.commit()
+    c.close()
+
+def add_review_index(conn: sqlite3.Connection, index: hnswlib.Index):
+    """
+    Adds a review index to the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        index (hnswlib.Index): The index to add.
+    """
+    logging.debug("Adding review index to SQLite database")
+
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO review_embeddings_hnsw_index (pickle)
+        VALUES (?)
+    ''', (pickle.dumps(index),))
+
+    conn.commit()
+    c.close()
+
+def get_any_description_embeddings_list(conn: sqlite3.Connection) -> List[List[float]]:
+    """
+    Gets the first description embeddings list from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        List[List[float]]: The description embeddings list.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT embedding FROM description_embeddings LIMIT 1
+    ''')
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return pickle.loads(results)
+
+def get_count_appids_with_description_embeddings(conn: sqlite3.Connection) -> int:
+    """
+    Gets the number of appids with description embeddings from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        int: The number of appids with description embeddings.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT COUNT(DISTINCT appid) FROM description_embeddings
+    ''')
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
+
+def get_count_appids_with_review_embeddings(conn: sqlite3.Connection) -> int:
+    """
+    Gets the number of appids with review embeddings from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        int: The number of appids with review embeddings.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT COUNT(DISTINCT appid) FROM review_embeddings
+    ''')
+    results = c.fetchone()[0]
+
+    c.close()
+
+    return results
+
+def get_description_embeddings_batch(conn: sqlite3.Connection, page_size: int = 1000) -> Iterator[List[Tuple[int, List[List[float]]]]]:
+    """
+    Gets a generator for description embeddings in batches.
+
+    Args:
+        page_size (int): The size of each batch.
+
+    Returns:
+        Iterator[List[Tuple[int, List[float]]]]: A generator for description embeddings in batches.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT appid, embedding FROM description_embeddings
+    ''')
+
+    while True:
+        results = c.fetchmany(page_size)
+
+        if not results:
+            break
+
+        yield [(appid, pickle.loads(embedding)) for appid, embedding in results]
+
+    c.close()
+
+def get_appids_with_reviews(conn: sqlite3.Connection) -> List[int]:
+    """
+    Gets the appids with reviews from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+
+    Returns:
+        Set[int]: The appids with reviews.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT DISTINCT appid FROM review_embeddings
+    ''')
+    results = c.fetchall()
+
+    c.close()
+
+    return [appid for appid, in results]
+
+def get_review_embeddings_for_appid(conn: sqlite3.Connection, appid: int) -> Dict[int, List[List[List[float]]]]:
+    """
+    Gets the review embeddings for an appid from the SQLite database.
+
+    Args:
+        conn (sqlite3.Connection): A connection to the SQLite database.
+        appid (int): The appid to get the review embeddings for.
+
+    Returns:
+        Dict[str, List[List[float]]]: The review embeddings for the appid.
+    """
+
+    c = conn.cursor()
+
+    c.execute(f'''
+        SELECT recommendationid, embedding FROM review_embeddings
+        WHERE appid = ?
+    ''', (appid,))
+    results = c.fetchall()
+
+    c.close()
+
+    return {recommendationid: pickle.loads(embedding) for recommendationid, embedding in results}
