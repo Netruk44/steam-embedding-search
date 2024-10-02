fe30d12dacbcb1484d7fd31dc3e074c14cf80eea
Daniel Perry <dperry@danieltperry.me>
2024-10-02 16:54:43 -0400

    Add label for number of results

--- 11_react-interface/src/App.js
+++ 11_react-interface/src/App.js
@@ -90,6 +90,7 @@ function App() {
             <h3>Advanced Options</h3>
             <label htmlFor="instruction">Instruction:</label>
             <input type="text" value={instruction} onChange={(event) => setInstruction(event.target.value)} placeholder="Represent a video game that has a description of:" />
+            <label htmlFor="numResults">Number of Results:</label>
             <input type="number" value={numResults} onChange={(event) => setNumResults(event.target.value)} min="1" max="100" /> <br />
             <label htmlFor="type">Type:</label>
             <select id="type" value={type} onChange={(event) => setType(event.target.value)}>
